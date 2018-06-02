e347da1f933b6233d7e1642218b7a4ae40b52024
Andrey Petrovich <baba_dro@mail.ru>
2018-06-02 13:03:56 +0300

    power of two

---
+++ Algorithms/2_1_Power_of_two/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_1_Power_of_two
+{
+    class Program
+    {
+        static bool IsPowerOfTwo(ulong x)
+        {
+            return (x != 0) && (x & (x - 1)) == 0;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
